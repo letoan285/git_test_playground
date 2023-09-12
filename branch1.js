8fdf158d514ffa0dc4d8ded2433d90e1b480142d
Le Van Toan <letoan285@gmail.com>
2023-09-12 10:30:32 +0700

    update code from Branch One

--- branch1.js
+++ branch1.js
@@ -3,3 +3,14 @@
 
 // 1. This is new code from test1 - branch one
 // 2. This is new code from test1 - branch one
+
+// This is function 1
+const getDetailOne = () => {
+    return 'detail page One'
+}
+
+
+// this is function 2
+const getListOne = () => {
+    return 'get list page One'
+}
